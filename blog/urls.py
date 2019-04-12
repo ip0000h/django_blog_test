946aee90c6db3321478410d17d0ac5a4d8cf8b93
Ivan Gorbachev <ip0000h@gmail.com>
2019-04-12 15:53:09 +0200

    crud views and templates for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import (BlogList, BlogView, IndexView, PostView,
-                    SubscriptionList)
+from .views import (BlogList, BlogView, IndexView,
+                    PostCreateView, PostDeleteView, PostUpdateView,
+                    PostView, SubscriptionList)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('list/', BlogList.as_view(), name='list'),
     path('subscriptions/', SubscriptionList.as_view(), name='subscriptions'),
     path('<int:blog_id>/', BlogView.as_view(), name='blog'),
+    path('myposts/', BlogView.as_view(), name='myposts'),
     path('post/<int:pk>/', PostView.as_view(), name='post'),
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     # path('subscribe/<int:blog_id>/', PostView.as_view(), name='subscribe'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import FeedPost, Post, Subscription
 
 
@@ -14,8 +15,12 @@ class IndexView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         """Return the user feed posts ordered by date."""
         with transaction.atomic():
-            user_subscriptions = Subscription.objects.values_list('blog').filter(user=self.request.user)
-            return Post.objects.filter(blog__in=user_subscriptions)
+            user_subscriptions = Subscription.objects.values_list('blog').filter(
+                user=self.request.user)
+            return Post.objects.filter(
+                blog__in=user_subscriptions
+                ).order_by('-created').values()
+
 
 class BlogView(LoginRequiredMixin, generic.ListView):
     template_name = 'blog.html'
@@ -23,8 +28,9 @@ class BlogView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         """Return the user blog posts ordered by date."""
+        blog_id = self.kwargs.get('blog_id') if self.kwargs.get('blog_id') else self.request.user.id
         return Post.objects.filter(
-            blog_id=self.kwargs['blog_id']
+            blog_id=blog_id
         ).order_by('-created').values()
 
 
@@ -43,10 +49,45 @@ class PostView(LoginRequiredMixin, generic.DetailView):
     context_object_name = 'post'
 
 
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    fields = ['title', 'post']
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('myposts')
+
+    def form_valid(self, form):
+        form.instance.blog = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    fields = ['title', 'post']
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('myposts')
+
+    def get_object(self):
+        """Return the Post instance that the view displays"""
+        return get_object_or_404(
+            Post,
+            pk=self.kwargs.get("pk"),
+            blog=self.request.user
+        )
+
+
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('myposts')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
 class SubscriptionList(LoginRequiredMixin, generic.ListView):
     template_name = 'subscriptions.html'
     context_object_name = 'subscriptions'
 
     def get_queryset(self):
         """Return the user's subscriptions"""
-        return Subscription.objects.filter(user=self.request.user).select_related()
+        return Subscription.objects.filter(
+            user=self.request.user).select_related()
